02303c52d93ca365ed59e85d29b957aae4ecc099
Edimilson Work <edimilson.work@gmail.com>
2015-12-08 01:06:31 -0200

    Inicio

---
+++ UniversoDesentupidora/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(UniversoDesentupidora.Startup))]
+namespace UniversoDesentupidora
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
